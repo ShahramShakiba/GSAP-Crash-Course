d4649f3cbd21914461b1cf0eb36b62898b4c4845
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-02 11:59:27 +0330

    Instantiate GSAP Animations Project

---
+++ GSAP-Animations/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import {
+  GsapFrom,
+  GsapFromTo,
+  GsapScrollTrigger,
+  GsapStagger,
+  GsapText,
+  GsapTimeline,
+  GsapTo,
+  Home,
+} from './pages';
+
+export default function App() {
+  return (
+    <div className="bg-black min-h-screen w-full">
+      <Router>
+        <Routes>
+          <Route path="/gsapto" element={<GsapTo />} />
+          <Route path="/gsapfrom" element={<GsapFrom />} />
+          <Route path="/gsapfromto" element={<GsapFromTo />} />
+          <Route path="/gsaptimeline" element={<GsapTimeline />} />
+          <Route path="/gsapstagger" element={<GsapStagger />} />
+          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+          <Route path="/gsaptext" element={<GsapText />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
