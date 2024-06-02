85a6acae01c55e26fc57199b33304765a9a8102c
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-02 16:49:24 +0330

    Grasp ScrollTrigger Plugin

--- GSAP-Animations/src/pages/GsapScrollTrigger.jsx
+++ GSAP-Animations/src/pages/GsapScrollTrigger.jsx
@@ -1,11 +1,12 @@
-import gsap from "gsap";
-import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/all";
+import gsap from 'gsap';
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
 
+//initiate ScrollTrigger at the top
 gsap.registerPlugin(ScrollTrigger);
 
-const GsapScrollTrigger = () => {
+export default function GsapScrollTrigger() {
   const scrollRef = useRef();
 
   useGSAP(
@@ -15,56 +16,65 @@ const GsapScrollTrigger = () => {
 
       boxes.forEach((box) => {
         gsap.to(box, {
-          x: 150 * (boxes.indexOf(box) + 4.5),
+          x: 200 * (boxes.indexOf(box) + 2),
           rotation: 360,
-          borderRadius: "100%",
+          borderRadius: '100%',
           scale: 1.5,
+          ease: 'power.in',
+          // duration: 2,
+
           scrollTrigger: {
             trigger: box,
-            start: "bottom bottom", // when the bottom of the box hits the bottom of the viewport
-            end: "top 20%", // end when the top of the box hits 20% from the top of the viewport
-            scrub: true, // scrubbing makes the animation smooth
+            start: 'top 70%',
+            //when the top of the box hits the 70% from the top of the viewport
+            end: 'top 50%',
+            //end when the top of the box hits 20% from the top of the viewport
+
+            scrub: 3,
+            //scrubbing makes the animation smooth - the higher value makes it smoother
+            pin: true, //pin the trigger element while active
+
+            // snap: {
+            //   snapTo:'.arrow', // snap to the closest label in the timeline
+            //    duration: { min: 0.2, max: 3 },
+            // },
           },
-          ease: "power1.inOut",
         });
       });
     },
-    { scope: scrollRef }
+    { scope: scrollRef } //it'll know when the animation has to happen
   );
 
   return (
     <main>
       <h1>GsapScrollTrigger</h1>
-
       <p className="mt-5 text-gray-500">
         Gsap Scroll Trigger is a plugin that allows you to create animations
         that are triggered by the scroll position of the page.
       </p>
-
       <p className="mt-5 text-gray-500">
         With ScrollTrigger, you can define various actions to be triggered at
         specific scroll points, such as starting or ending an animation,
         scrubbing through animations as the user scrolls, pinning elements to
-        the screen, and more.{" "}
+        the screen, and more.{' '}
       </p>
 
       <p className="mt-5 text-gray-500">
-        Read more about the{" "}
+        Read more about the{' '}
         <a
           href="https://gsap.com/docs/v3/Plugins/ScrollTrigger/"
           target="_blank"
           rel="noreferrer noopener nofollow"
         >
           gsap scroll trigger
-        </a>{" "}
+        </a>{' '}
         method.
       </p>
 
-      <div className="w-full h-[70vh] flex justify-center items-center flex-col">
+      <div className="w-full h-[70vh] flex justify-center items-center flex-col arrow">
         <p className="text-center text-gray-500">
           Scroll down to see the animation
         </p>
-
         <svg
           className="animate-bounce mt-5"
           xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +95,7 @@ const GsapScrollTrigger = () => {
       <div className="mt-20 w-full h-screen" ref={scrollRef}>
         <div
           id="scroll-pink"
-          className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
+          className="scroll-box w-20 h-20 rounded-lg bg-pink-500 mb-10"
         />
         <div
           id="scroll-orange"
@@ -94,6 +104,4 @@ const GsapScrollTrigger = () => {
       </div>
     </main>
   );
-};
-
-export default GsapScrollTrigger;
+}
